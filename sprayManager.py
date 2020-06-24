84f1cce69781626739a3ff17ef53141a9dcb4874
seollove79 <seollove@gmail.com>
2020-06-24 22:22:44 +0900

    test

--- sprayManager.py
+++ sprayManager.py
@@ -1,22 +1,21 @@
 from tkinter import *
 from RangerFinder import *
 
-root = tk.Tk()
+_root = Tk()
 
 class SprayManagerWindow:
-    def __init__(self):
-        global root
-
-        self.frame = Frame(root, width=1280, height=800, relief='solid',bd=1)
+    def init(self):
+        self.frame = Frame(_root, width=1280, height=800, relief='solid',bd=1)
         self.frame.place(x=10, y=10)
 
-        self.label_distance = Label(frame, textvariable=distanceVar, relief=RAISED)
         self.distanceBar = StringVar()
         self.distanceVar.set("Show me distance")
-        self.label_distance.pack()
 
-        self.button = Button(window, overrelief="solid", width=15, command=self.Update, repeatdelay=1000, repeatinterval=100)
-        self.button.pack()
+        label_distance = Label(self.frame, textvariable=distanceVar, relief=RAISED)
+        label_distance.pack()
+
+        button = Button(self.frame, overrelief="solid", width=15, command=self.update, repeatdelay=1000, repeatinterval=100)
+        button.pack()
 
         self.rangeFinder = RangerFinder()
 
@@ -24,17 +23,21 @@ class SprayManagerWindow:
         distance = self.rangeFinder.read()
         output = str(distance)
         self.distanceVar.set(output)
-        self.root.after(10, refresher) # every second...
+        _root.after(10, refresher) # every second...
 
     def start(self):
-        self.rangeFinder.start();
+        return
 
-#    def stop(self):
+    def stop(self):
+        return
+
+    def update(self):
+        return
 
     def unload(self):
-        self.root.mainloop()
+        _root.mainloop()
 
-manager = SprayManagerWindow()
-manager.refresher()
+_manager = SprayManagerWindow()
+_manager.refresher()
 
-root.mainloop()
+_root.mainloop()
