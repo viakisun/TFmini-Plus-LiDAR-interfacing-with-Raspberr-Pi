142edc64095dcb270a797ceb03af5e342d8b826c
seollove79 <seollove@gmail.com>
2020-06-25 17:07:51 +0900

    test

--- sprayManager.py
+++ sprayManager.py
@@ -43,6 +43,34 @@ class SprayManagerWindow:
         _root.attributes("-fullscreen", False)
         return "break"
 
+
+class MyDialog:
+    def __init__(self, parent, valor, title, labeltext = '' ):
+        self.valor = valor
+ 
+        self.top = Toplevel(parent)
+        self.top.transient(parent)
+        self.top.grab_set()
+        if len(title) > 0: self.top.title(title)
+        if len(labeltext) == 0: labeltext = 'Valor'
+        Label(self.top, text=labeltext).pack()
+        self.top.bind("<Return>", self.ok)
+        self.e = Entry(self.top, text=valor.get())
+        self.e.bind("<Return>", self.ok)
+        self.e.bind("<Escape>", self.cancel)
+        self.e.pack(padx=15)
+        self.e.focus_set()
+        b = Button(self.top, text="OK", command=self.ok)
+        b.pack(pady=5)
+ 
+    def ok(self, event=None):
+        print "Has escrito ...", self.e.get()
+        self.valor.set(self.e.get())
+        self.top.destroy()
+ 
+    def cancel(self, event=None):
+        self.top.destroy()
+
 _manager = SprayManagerWindow()
 _manager.refresher()
 
