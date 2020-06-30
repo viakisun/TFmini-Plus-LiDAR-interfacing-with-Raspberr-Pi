67fdea48ee107dbd013cb92fd92ae06ec0aa8ae8
viahoon <hoon@viasofts.com>
2020-07-01 05:11:55 +0900

    test

--- StartPage.py
+++ StartPage.py
@@ -74,7 +74,7 @@ class StartPage(tk.Frame):
 
 
     def startAuto(self):
-        if self.controller.SprayMode == SprayMode.AUTO :
+        if self.controller.sprayMode == SprayMode.AUTO :
             self.sprayByTime()
             threading.Timer(5, startAuto).start()
 
