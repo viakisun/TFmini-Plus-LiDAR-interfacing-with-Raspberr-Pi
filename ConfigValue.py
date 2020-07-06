e1968178c8eb58dcc66668722c412eace46fc896
viahoon <hoon@viasofts.com>
2020-07-06 20:25:53 +0900

    just backup

--- ConfigValue.py
+++
@@ -1,5 +0,0 @@
-class ConfigValue :
-    ConfigValue.distanceModeSprayTime = None
-    ConfigValue.distanceModeDetectDistance = None
-    ConfigValue.autoModeCycleTime = None
-    ConfigValue.autoModeSprayTime = None
