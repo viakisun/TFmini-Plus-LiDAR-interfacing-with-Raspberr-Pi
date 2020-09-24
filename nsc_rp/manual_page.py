392f52c9d4cead8c0161d12d1f6bfabb9becf2e6
viahoon <hoon@viasofts.com>
2020-09-24 10:23:36 +0900

    라즈베리파이버젼 수동모드에 솔레노이드밸브 조작 코드 추가

--- nsc_rp/manual_page.py
+++ nsc_rp/manual_page.py
@@ -16,6 +16,7 @@ class ManualPage(SettingPage):
         self.init_UI()
         self.init_spray()
         self.change_spray_btn()
+        self.out_valve_start_time = None
 
     def init_UI(self):
         self.img_btn_spray_on = tk.PhotoImage(file='/home/pi/devwork/nsc/nsc_rp/images/btn_spray_on.png')
@@ -34,16 +35,21 @@ class ManualPage(SettingPage):
         if super().is_linux_system():
             GPIO.setmode(GPIO.BCM)
             GPIO.output(ConfigValue.SPRAY_WPI_NUM, False)
+            GPIO.output(ConfigValue.VALVE_WPI_NUM, False)
 
     def spray_on(self):
         if super().is_linux_system():
             GPIO.output(ConfigValue.SPRAY_WPI_NUM, True)
+            GPIO.output(ConfigValue.VALVE_WPI_NUM, False)
+
         self.spray_on_check = True
         self.change_spray_btn()
 
     def spray_off(self):
         if super().is_linux_system():
             GPIO.output(ConfigValue.SPRAY_WPI_NUM, False)
+            self.out_valve_start_time = time.time()
+            self.on_out_valve()
         self.spray_on_check = False
         self.change_spray_btn()            
 
@@ -54,3 +60,13 @@ class ManualPage(SettingPage):
         else:
             self.btn_spray_on.configure(image = self.img_btn_spray_on)
             self.btn_spray_off.configure(image = self.img_btn_spray_off_sel)
+
+    def on_out_valve(self):
+        if time.time() - self.out_valve_start_time < ConfigValue.VALVE_ON_TIME:
+            if platform.system() == "Linux":
+                GPIO.output(ConfigValue.VALVE_WPI_NUM, True)
+        else:
+            if platform.system() == "Linux":
+                GPIO.output(ConfigValue.VALVE_WPI_NUM, False)
+            return True
+        threading.Timer(0.1, self.on_out_valve).start()
